925b9b3ddc1621cadb60512fcd662d06d79c15e6
raydabbah <raydabbah@gmail.com>
2018-12-02 23:57:30 -0500

    wrap code in block scope to use const instead of var

--- showPass.js
+++ showPass.js
@@ -1,10 +1,14 @@
-var passwordFields = Array.from(document.querySelectorAll('[type="password"]'));
-var shownPasswordFields = Array.from(
-  document.querySelectorAll('[type="show-pass"]')
-);
-passwordFields.forEach(function(el) {
-  el.setAttribute("type", "show-pass");
-});
-shownPasswordFields.forEach(function(el) {
-  el.setAttribute("type", "password");
-});
+{
+  const passwordFields = Array.from(
+    document.querySelectorAll('[type="password"]')
+  );
+  const shownPasswordFields = Array.from(
+    document.querySelectorAll('[type="show-pass"]')
+  );
+  passwordFields.forEach(function(el) {
+    el.setAttribute("type", "show-pass");
+  });
+  shownPasswordFields.forEach(function(el) {
+    el.setAttribute("type", "password");
+  });
+}
